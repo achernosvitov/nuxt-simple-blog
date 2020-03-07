55cb95e59938b3f8b7ff9b4361a9bb9c03793a35
Александр Черносвитов <chernick71@gmail.com>
2020-03-08 01:17:31 +0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-navbar tag="header" variant="light" type="light" toggleable="lg" sticky>
+      <b-navbar-brand to="/">
+        <img src="https://placekitten.com/g/30/30" alt="Kitten">
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse" />
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">
+            Главная
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <nuxt />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container class="mt-4">
+    <b-card
+      title="Lorem ipsum dolor"
+      img-src="https://picsum.photos/1200/600/"
+      img-alt="Image"
+      tag="article"
+      class="mb-2"
+    >
+      <b-card-text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi laborum ad reprehenderit unde ex sed accusantium distinctio, nisi voluptatum assumenda, eum doloremque hic officiis ratione dignissimos. Accusantium, quidem qui?
+      </b-card-text>
+    </b-card>
+  </b-container>
+</template>
